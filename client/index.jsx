fe140a78632b19e1cf554b7215801cec13066bb1
Alexandra Sandoval <alexandoval13@gmail.com>
2020-11-24 18:05:44 -0800

    Refactor client render method

--- client/index.jsx
+++ client/index.jsx
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 import App from './App.jsx';
 
 
-if (document.getElementById('app')) {
-  ReactDOM.render(<App />, document.getElementById('app'));
-} else {
+if (document.getElementById('service1')) {
   ReactDOM.render(<App />, document.getElementById('service1'));
+} else {
+  ReactDOM.render(<App />, document.getElementById('app'));
 }
